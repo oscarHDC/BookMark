b6b694cbc5ac311d6ac601c2fbf6e6dcd5d4e673
oscarHDC <101225451+oscarHDC@users.noreply.github.com>
2022-10-16 14:40:40 +0200

    Update navigation.js

--- script/navigation.js
+++ script/navigation.js
@@ -1,6 +1,6 @@
 const btnNav = document.querySelector(".main-nav-mobile");
 const nav = document.querySelector(".main-nav");
-const close = document.querySelector(".close");
+
 
 btnNav.addEventListener("click", function () {
   nav.classList.toggle("active");
